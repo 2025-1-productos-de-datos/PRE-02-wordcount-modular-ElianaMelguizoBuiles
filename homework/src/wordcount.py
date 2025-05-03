b9467a1bd7be0d8f381aef076a33200cb7ced812
Melguizo Builes, Eliana <emmelguizob@unal.edu.co>
2025-05-03 11:32:39 -0500

    copia

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -2,20 +2,22 @@
 
 from homework.src._internals.count_words import count_words
 from homework.src._internals.read_all_lines import read_all_lines
-from homework.src._internals.reprocess_lines import reprocess_lines
+from homework.src._internals.preprocess_lines import reprocess_lines
 from homework.src._internals.split_in_words import split_in_words
 
 from ._internals.write_count_words import write_count_words
 
 
 def main():
+    input_folder = "data/input"
+    output_folder = "data/output"
 
    
-    all_lines = read_all_lines()
+    all_lines = read_all_lines(input_folder)
     all_lines = reprocess_lines(all_lines)
     words = split_in_words(all_lines)
     counter = count_words(words)
-    write_count_words(counter)
+    write_count_words(counter,output_folder)
 
 if __name__ == "__main__":
     main()
